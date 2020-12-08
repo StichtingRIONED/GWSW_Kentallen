4e4efb9686c658987b7578f91e51ae887e12b4cc
marinus vonhof <marinus.vonhof@live.nl>
2020-12-08 14:34:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,37 +4,36 @@ var respecConfig =
   specStatus: "design",
   //specType: "HR",             // Handreiking
   //format: "markdown",         // Altijd "markdown"     
-  shortName: "GWSW-2.0", 	      // Wordt gebruikt in de document URL
+  shortName: "GWSW-Kentallen", 	      // Wordt gebruikt in de document URL
   license: "cc0",
    
   editors: [{
-    name: "Michel Böhms",
-    url: "mailto:michel.bohms@tno.nl",
-    company: "TNO",
-    companyURL: "https://www.tno.nl"
-  },
-  {
     name: "Eric Oosterom",
     url: "mailto:eric.oosterom@rioned.org",
     company: "Stichting RIONED",
     companyURL: "https://www.riool.net"
   }],
   authors: [{
+    name: "Jordie Netten",
+    url: "mailto:jordie.netten@nelen-schuurmans.nl",
+    company: "Nelen & Schuurmans",
+    companyURL: "https://www.nelen-schuurmans.nl/"
+  },{
     name: "Marinus Vonhof",
     url: "mailto:marinus.vonhof@rioned.org",
     company: "Stichting RIONED",
     companyURL: "https://www.riool.net"
   }],
   otherLinks: [{
-    key: "GitHub", href: "https://github.com/stichtingrioned/GWSW_2.0/"
+    key: "GitHub", href: "https://github.com/stichtingrioned/GWSW_Kentallen/"
   }],
   //github: "https://github.com/StichtingRIONED/GWSW"
   //-- Repositorynaam/issues op GitHub ------------------------------------------------
   //issueBase: "https://github.com/StichtingRIONED/GWSW/issues/",
 
-  thisVersion: "https://stichtingrioned.github.io/GWSW_2.0/",
+  thisVersion: "https://stichtingrioned.github.io/GWSW_Kentallen/",
   latestVersion: "", // gepubliceerde versie
-  edDraftURI: "https://stichtingrioned.github.io/GWSW_2.0/", // werkversie
+  edDraftURI: "https://stichtingrioned.github.io/GWSW_Kentallen/", // werkversie
 
   maxTocLevel: 3, // Aantal niveau's ToC, default is 0
 };
